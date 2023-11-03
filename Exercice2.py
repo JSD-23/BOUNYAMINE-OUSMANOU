1c4a9a808fae31058ad36ab555319c79f82fc449
BounyLeCracker <ousmanoubounyamine@gmail.com>
2023-11-03 10:36:40 +0100

    Update Exercice2.py
    Exercice 2 ok

--- Exercice2.py
+++ Exercice2.py
@@ -1,3 +1,15 @@
 def maxProfit(prix):
-    
-    return 0
+    size = len(prix)
+    if size <= 1:
+        return 0
+    profit = 0
+    acheter = prix[0]
+
+    for i in range(1, size):
+        vendre = prix[i]
+        if vendre > acheter:
+            gain = vendre - acheter
+            profit = max(profit, gain)
+        else:
+            acheter = vendre
+    return profit
